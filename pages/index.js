b6be15b2c09f668dbebe4126c94f173ad8241f62
Dax Classix <dax.classix@gmail.com>
2016-08-12 22:47:31 +0800

    add page header / footer

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react'
 
-import Container from 'muicss/lib/react/container'
-import Button from 'muicss/lib/react/button'
+import Header from '../components/header'
 
 export default class Homepage extends Component {
   render () {
     return (
-      <Container fluid className="mui--text-center mui--bg-primary-light">
-        <h1>Ethereum <span>Classic</span></h1>
-        <h2>Decentralised, Immutable, Unstoppable</h2>
-        <Button>Download Client</Button>
-        <Button>Join Slack</Button>
-        <Button>Support Project</Button>
-      </Container>
+      <article className="homepage">
+        <Header />
+      </article>
     )
   }
 }
