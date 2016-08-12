ecf70b831f09ed0d7dfdce7e7a2eb5a5e25aa2b2
Dax Classix <dax.classix@gmail.com>
2016-08-12 12:34:04 +0800

    implement MUI css boilerplate

--- wrappers/md.jsx
+++ wrappers/md.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import DocumentTitle from 'react-document-title'
+import Helmet from 'react-helmet'
 import { config } from 'config'
 
 module.exports = React.createClass({
@@ -12,12 +12,12 @@ module.exports = React.createClass({
     const post = this.props.route.page.data
 
     return (
-      <DocumentTitle title={`${post.title} | ${config.siteTitle}`}>
+      <Helmet title={`${post.title} | ${config.siteTitle}`}>
         <div className="markdown">
           <h1>{post.title}</h1>
           <div dangerouslySetInnerHTML={{ __html: post.body }} />
         </div>
-      </DocumentTitle>
+      </Helmet>
     )
   },
 })
