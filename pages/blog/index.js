e6cdc68081408d82d11017901c6918e952c7930a
Dax Classix <dax.classix@gmail.com>
2016-08-14 19:51:48 +0800

    blog wip

---
+++ pages/blog/index.js
@@ -0,0 +1,13 @@
+import React, { PropTypes, Component } from 'react'
+
+export default class Blog extends Component {
+  render () {
+    return (
+      <h1>Let's read some interesting stories</h1>
+    )
+  }
+}
+
+Blog.propTypes = {
+
+}
