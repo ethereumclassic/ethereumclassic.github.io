22119fa4d7cd76ee391e9f8369dc4fcc68deecd1
Dax Classix <dax.classix@gmail.com>
2016-08-16 17:54:58 +0800

    add local pdf asset

--- components/home/why-classic.js
+++ components/home/why-classic.js
@@ -1,9 +1,8 @@
 import React, { Component, PropTypes } from 'react'
+import { prefixLink } from 'gatsby-helpers'
 
 import Container from 'muicss/lib/react/container'
-import Row from 'muicss/lib/react/row'
 import Col from 'muicss/lib/react/col'
-import Button from 'muicss/lib/react/button'
 
 import IconDecentralised from 'react-icons/lib/ti/arrow-maximise'
 import IconFinality from 'react-icons/lib/ti/link'
@@ -24,17 +23,10 @@ export default class WhyClassic extends Component {
           <div dangerouslySetInnerHTML={{ __html: whyClassic.body }} />
           <a
             className="mui-btn mui-btn mui-btn--raised mui-btn--primary"
-            href="https://ethereumclassic.github.io/assets/ETC_Declaration_of_Independence.pdf"
+            href={prefixLink('/assets/ETC_Declaration_of_Independence.pdf')}
             target="_blank"
           >
             Read the Declaration
-          </a>
-          <br />
-          <a
-            className="mui-btn mui-btn mui-btn--raised mui-btn--primary"
-            href="#fork-timeline"
-          >
-            Learn about the Fork
           </a>
           <br /><br />
         </Col>
