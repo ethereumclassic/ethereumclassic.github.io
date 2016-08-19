f769d38b3f7f468e6f6b81be608028d854f1ca8c
dax-classix <dax.classix@gmail.com>
2016-08-19 11:08:57 +0800

    modify social media

--- components/social-links.js
+++ components/social-links.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+import Dropdown from 'muicss/lib/react/dropdown'
+import DropdownItem from 'muicss/lib/react/dropdown-item'
+
 import Github from 'react-icons/lib/fa/github-alt'
 import Twitter from 'react-icons/lib/fa/twitter'
 import Slack from 'react-icons/lib/fa/slack'
@@ -10,14 +13,14 @@ import Irc from 'react-icons/lib/fa/terminal'
 export default class SocialLinks extends Component {
   render () {
     return (
-      <span className="social-links">
-        <a href="https://github.com/ethereumproject" target="_blank"><Github /></a>
-        <a href="https://twitter.com/eth_classic" target="_blank"><Twitter /></a>
-        <a href="http://ethereumclassic.herokuapp.com" target="_blank"><Slack /></a>
-        <a href="https://reddit.com/r/ethereumclassic" target="_blank"><Reddit /></a>
-        <a href="https://telegram.me/ethclassic" target="_blank"><Telegram /></a>
-        <a href="irc://irc.freenode.net/ethereumclassic" target="_blank"><Irc /></a>
-      </span>
+      <Dropdown label="Social Media" className="social-links">
+        <DropdownItem link="https://github.com/ethereumproject" target="_blank"><Github /> Github</DropdownItem>
+        <DropdownItem link="https://twitter.com/eth_classic" target="_blank"><Twitter /> Twitter</DropdownItem>
+        <DropdownItem link="http://ethereumclassic.herokuapp.com" target="_blank"><Slack /> Slack</DropdownItem>
+        <DropdownItem link="https://reddit.com/r/ethereumclassic" target="_blank"><Reddit /> Reddit</DropdownItem>
+        <DropdownItem link="https://telegram.me/ethclassic" target="_blank"><Telegram /> Telegram</DropdownItem>
+        <DropdownItem link="irc://irc.freenode.net/ethereumclassic" target="_blank"><Irc /> IRC</DropdownItem>
+      </Dropdown>
     )
   }
 }
