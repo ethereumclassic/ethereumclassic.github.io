6c4814fb8d4ff44e3ce5a743259d70d29a2d5a55
dax-classix <dax.classix@gmail.com>
2016-08-21 23:26:55 +0800

    add new social media links and, fixes #44 #42 #41

--- components/social-links.js
+++ components/social-links.js
@@ -9,17 +9,21 @@ import Slack from 'react-icons/lib/fa/slack'
 import Reddit from 'react-icons/lib/fa/reddit-alien'
 import Telegram from 'react-icons/lib/fa/paper-plane'
 import Irc from 'react-icons/lib/fa/terminal'
+import Forum from 'react-icons/lib/fa/comments'
 
 export default class SocialLinks extends Component {
   render () {
     return (
-      <Dropdown label="Social Media" className="social-links">
+      <Dropdown label="Social Links" className="social-links">
         <DropdownItem link="https://github.com/ethereumproject" target="_blank"><Github /> Github</DropdownItem>
-        <DropdownItem link="https://twitter.com/eth_classic" target="_blank"><Twitter /> Twitter</DropdownItem>
+        <DropdownItem link="https://twitter.com/ethereumclassic" target="_blank"><Twitter /> @etheruemclassic</DropdownItem>
+        <DropdownItem link="https://twitter.com/eth_classic" target="_blank"><Twitter /> @eth_classic</DropdownItem>
         <DropdownItem link="http://ethereumclassic.herokuapp.com" target="_blank"><Slack /> Slack</DropdownItem>
         <DropdownItem link="https://reddit.com/r/ethereumclassic" target="_blank"><Reddit /> Reddit</DropdownItem>
         <DropdownItem link="https://telegram.me/ethclassic" target="_blank"><Telegram /> Telegram</DropdownItem>
+        <DropdownItem link="http://telegram.me/etcrussia" target="_blank"><Telegram /> Telegram (Russian)</DropdownItem>
         <DropdownItem link="irc://irc.freenode.net/ethereumclassic" target="_blank"><Irc /> IRC</DropdownItem>
+        <DropdownItem link="https://ethereumclassictalk.org" target="_blank"><Forum /> Ethereum Classic Forums</DropdownItem>
       </Dropdown>
     )
   }
