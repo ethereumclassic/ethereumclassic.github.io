aba027599545525b576473beaa1fef02f106b1d7
Anthony <ajlusardi@gmail.com>
2017-12-08 14:22:19 -0500

    Update social links

--- components/social-links.js
+++ components/social-links.js
@@ -16,13 +16,11 @@ export default class SocialLinks extends Component {
   render () {
     return (
       <Dropdown label="Social Links" className="social-links">
-        <DropdownItem link="http://ethereumclassic.herokuapp.com" target="_blank"><Slack className="icon" /> Slack</DropdownItem>
+        <DropdownItem link="https://discord.gg/HW4GckH" target="_blank"><Slack className="icon" />Discord</DropdownItem>
         <DropdownItem link="https://reddit.com/r/ethereumclassic" target="_blank"><Reddit className="icon" /> Reddit</DropdownItem>
         <DropdownItem link="https://twitter.com/eth_classic" target="_blank"><Twitter className="icon" /> @eth_classic</DropdownItem>
         <DropdownItem link="https://github.com/ethereumproject" target="_blank"><Github className="icon" /> Github</DropdownItem>
-        <DropdownItem link="https://telegram.me/ethclassic" target="_blank"><Telegram className="icon" /> Telegram</DropdownItem>
         <DropdownItem link="http://telegram.me/etcrussia" target="_blank"><Telegram className="icon" /> Telegram (Russian)</DropdownItem>
-        <DropdownItem link="https://ethereumclassictalk.org" target="_blank"><Forum className="icon" /> Ethereum Classic Forums</DropdownItem>
         <DropdownItem link="http://webchat.freenode.net?channels=ethereumclassic&uio=d4" target="_blank"><Irc className="icon" /> IRC</DropdownItem>
         <DropdownItem link="https://www.linkedin.com/company/ethereum-classic" target="_blank"><Linkedin className="icon" /> LinkedIn</DropdownItem>
       </Dropdown>
