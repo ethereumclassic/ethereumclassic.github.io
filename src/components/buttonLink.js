8f641c48b009d05e70e72148760b4b197bf13a61
Chris Hitchcott <hitchcott@gmail.com>
2019-10-29 12:30:04 +0700

    Attempt to standardize component filenames

--- src/components/buttonLink.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Link from './link';
-
-const ButtonLink = ({ className, ...props }) => {
-  return <Link className={`button-link ${className}`} {...props} />;
-};
-
-export default ButtonLink;
--- src/components/spacer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Spacer = ({ size }) => {
-  return <div style={{ marginTop: size || '3em' }} />;
-};
-
-export default Spacer;
