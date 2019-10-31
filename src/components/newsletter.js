98dc107c10a969803cc758684f8d416515a54228
Chris Hitchcott <hitchcott@gmail.com>
2019-10-31 15:19:35 +0700

    Add newsletter form (waiting on CORS to complete)

---
+++ src/components/newsletter.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+// TODO test when CORS enabled or switch mailchimp
+
+const endpoint = 'https://wordpress.ethereumclassic.org/newsletter-thank-you/';
+
+class Newsletter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false, submitted: false };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  async onSubmit(e) {
+    e.preventDefault();
+    const email = new FormData(e.target).get('email');
+    this.setState({ loading: true });
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      body: {
+        input_1: email,
+        gform_target_page_number_2: 0,
+        gform_source_page_number_2: 1,
+        is_submit_2: 1,
+        gform_submit: 2
+      }
+    });
+    this.setState({ loading: false });
+  }
+
+  render() {
+    const { loading, error, submitted } = this.state;
+    return (
+      <>
+        <h3>ETC Newsletter</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="fields">
+            <div className="field">
+              <input type="email" name="email" id="email" placeholder="Email" />
+            </div>
+          </div>
+          <ul className="actions">
+            <li>
+              <input
+                type="submit"
+                value="Sign Up"
+                className="button primary"
+                disabled={loading ? 'disabled' : null}
+              />
+            </li>
+          </ul>
+        </form>
+      </>
+    );
+  }
+}
+
+export default Newsletter;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Navigation from '~components/navigation';
 import Landing from '~components/landing';
+import Footer from '~components/footer';
 
 const Index = ({ pageContext: { i18n } }) => (
   <>
     <Navigation />
     <div id="wrapper">
       <Landing i18n={i18n} />
+      <Footer />
     </div>
   </>
 );
