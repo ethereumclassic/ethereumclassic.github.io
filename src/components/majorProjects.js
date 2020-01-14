2a5fa85eec2ee084d775291b477c7267433b2f3f
Chris Hitchcott <hitchcott@gmail.com>
2020-01-14 19:10:30 +0700

    Convert clients and projects into YAML arrays; add links; fixes #275

--- src/components/majorProjects.js
+++ src/components/majorProjects.js
@@ -5,29 +5,18 @@ import LinkSection from '~components/linkSection';
 const MajorProjects = ({ i18n }) => {
   return (
     <>
-      <h2>{i18n.projectsTitle}</h2>
-      <LinkSection
-        title={i18n.multiGeth}
-        text={i18n.multiGethText}
-        to="https://github.com/ethoxy/multi-geth"
-      />
-      <LinkSection
-        to="https://github.com/ETCDEVTeam/sidekick-doc"
-        title={i18n.sidechains}
-        text={i18n.sidechainsText}
-      />
-
-      <LinkSection
-        to="https://github.com/ETCDEVTeam/sputnikvm"
-        title={i18n.sputnikVM}
-        text={i18n.sputnikVMText}
-      />
-
-      <LinkSection
-        to="https://github.com/ETCDEVTeam/emerald-platform"
-        title={i18n.emeraldPlatform}
-        text={i18n.emeraldPlatformText}
-      />
+      <section>
+        <h2>{i18n.clients}</h2>
+        {i18n.yaml.clients.map(({ key, title, link, text }) => (
+          <LinkSection {...{ key, title, text, to: link }} />
+        ))}
+      </section>
+      <section>
+        <h2>{i18n.projectsTitle}</h2>
+        {i18n.yaml.projects.map(({ key, title, link, text }) => (
+          <LinkSection {...{ key, title, text, to: link }} />
+        ))}
+      </section>
     </>
   );
 };
