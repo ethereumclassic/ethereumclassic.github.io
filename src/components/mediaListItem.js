abd572ad67c1ddbe04e78c925edbba5bceb45cc7
Chris Hitchcott <hitchcott@gmail.com>
2020-02-11 12:17:01 +0700

    Add old media links; fixes #349

--- src/components/mediaListItem.js
+++ src/components/mediaListItem.js
@@ -3,12 +3,12 @@ import React from 'react';
 import Link from './link';
 import Translate from './translate';
 
-const BlogListItem = ({ key, date, title, source, link, text }) => {
+const BlogListItem = ({ key, date, title, author, source, link, text }) => {
   return (
     <div key={key} className="blog-item">
       <h4>
         <Translate date={date} />
-        <small>{` ${source || ''}`}</small>
+        <small>{` ${author || ''}${author && source ? ',' : ''} ${source || ''}`}</small>
       </h4>
       <h3>
         <Link to={link} text={title} />
