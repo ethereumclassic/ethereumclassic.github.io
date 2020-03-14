c6b872b919299cd4e2b7d850d55d33ddcee7aaa6
Chris Hitchcott <hitchcott@gmail.com>
2020-03-14 19:26:32 +0700

    Update wallets, exchanges, mining info, roadmap text #363 #368 #365

--- src/routes/ecosystem/wallets.js
+++ src/routes/ecosystem/wallets.js
@@ -4,20 +4,17 @@ import { groupItems } from '../../utils';
 import html from '../../i18n/html';
 
 import SubPageLayout from '~components/subPageLayout';
-import LinkGroup from '~components/linkGroup';
 import ButtonGroup from '~components/buttonGroup';
 
 const Exchanges = ({ pageContext: { i18n } }) => {
   const items = groupItems(i18n.yaml.items);
   return (
     <SubPageLayout i18n={i18n}>
-      {html(i18n.browserWallets)}
-      <ButtonGroup header={html(i18n.interactiveWallets)} items={items.interactive} />
-      <ButtonGroup header={html(i18n.standaloneWallets)} items={items.standalone} />
+      <ButtonGroup header={html(i18n.webWallets)} items={items.web} />
       <ButtonGroup header={html(i18n.extensionWallets)} items={items.extension} />
-      <ButtonGroup header={html(i18n.hardwareWallets)} items={items.hardware} />
       <ButtonGroup header={html(i18n.softwareWallets)} items={items.software} />
-      <LinkGroup header={html(i18n.otherWallets)} items={items.other} />
+      <ButtonGroup header={html(i18n.hardwareWallets)} items={items.hardware} />
+      <ButtonGroup header={html(i18n.otherWallets)} items={items.other} />
     </SubPageLayout>
   );
 };
