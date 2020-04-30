5eeb7ed170163d5740a925d700b3a44aeb05eaf4
Chris Hitchcott <hitchcott@gmail.com>
2020-04-30 17:35:56 +0100

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-const Default = () => <div>Page not found</div>;
+import logo from '../assets/images/etc-black.svg';
+
+const Default = () => (
+  <div className="page-missing">
+    <img src={logo} alt="ETC Logo" />
+    <h2>404</h2>
+    <h1>Page Not Found</h1>
+    <Link to="/" className="button-link">
+      <i className="fas fa-chevron-left left" />
+      Go Home
+      <i className="fas fa-home right" />
+    </Link>
+    <hr />
+    <small>https://ethereumclassic.org</small>
+  </div>
+);
 
 export default Default;
