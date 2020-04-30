976fe820f6ecf53c53a6f5ea319941d283f76aa6
Chris Hitchcott <hitchcott@gmail.com>
2020-04-30 20:27:26 +0100

    Add date to blog articles

--- src/components/LayoutMarkdown.js
+++ src/components/LayoutMarkdown.js
@@ -4,6 +4,7 @@ import { MDXProvider } from '@mdx-js/react';
 
 import LayoutPage from './LayoutPage';
 import Link from './Link';
+import DateFormatter from './DateFormatter';
 
 const components = {
   a: ({ href, ...props }) => <Link to={href} {...props} />,
@@ -18,10 +19,18 @@ const LayoutMarkdown = props => {
   const {
     data: {
       markdown: { body }
-    }
+    },
+    pageContext: { i18n }
   } = props;
   return (
     <LayoutPage {...props}>
+      {i18n.date && (
+        <h3>
+          <DateFormatter>{i18n.date}</DateFormatter>
+          {'  '}
+          <small>{i18n.author}</small>
+        </h3>
+      )}
       <MDXProvider components={components}>
         <MDXRenderer>{body}</MDXRenderer>
       </MDXProvider>
