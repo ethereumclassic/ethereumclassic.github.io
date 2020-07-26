b73c72e8cbf31721e7c563e6f0e1b6f8a5ea2a85
Chris Hitchcott <hitchcott@gmail.com>
2020-07-26 17:15:39 +0200

    Use css for hidden menus and for js-disabled and tab navigation

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,6 +1,5 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-import React, { useState } from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
 
 import Menu from './Menu';
 
@@ -9,18 +8,15 @@ const MainMenu = ({
     globals: { mainMenu }
   }
 }) => {
-  const [hidden, setHidden] = useState(true);
-  function toggle() {
-    setHidden(!hidden);
-  }
   return (
     <div className="main-menu">
-      <div className={`overlay ${hidden ? 'hidden' : ''}`} onClick={toggle}>
-        <Menu items={mainMenu} onClick={toggle} />
-      </div>
-      <div className={`hamburger ${!hidden ? 'selected' : ''}`} onClick={toggle}>
+      <input id="menu-toggle" type="checkbox" />
+      <Menu items={mainMenu} />
+      <div className="hamburger">
         <i className="fas fa-bars" />
       </div>
+      <label htmlFor="menu-toggle" className="toggle" />
+      <div className="overlay" />
     </div>
   );
 };
