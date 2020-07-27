4ec216c173e8c0a6485024df9878913fc0fe7c76
Chris Hitchcott <hitchcott@gmail.com>
2020-07-27 17:41:07 +0200

    Add dark theme switcher

---
+++ src/components/DarkMode.js
@@ -0,0 +1,29 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+
+import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+
+const DarkMode = ({ i18n }) => {
+  // get then
+  const [on, setOn] = useState();
+  // return [on, setOn];
+  function toggle() {
+    setOn(!on);
+  }
+  useEffect(() => {
+    console.log(window.matchMedia('(prefers-color-scheme: dark)').matches);
+  }, []);
+
+  return (
+    <>
+      <Helmet>
+        <body className={`${on ? 'dark-theme' : 'light-theme'} ${i18n.bodyClass || ''}`} />
+      </Helmet>
+      <div className="dark-mode" onClick={toggle}>
+        <i className={`fas fa-${on ? 'moon' : 'sun'}`} />
+      </div>
+    </>
+  );
+};
+
+export default DarkMode;
