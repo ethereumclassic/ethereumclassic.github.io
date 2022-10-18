b7a1ed1f65a735fd7774de1de223d0e491352580
IstoraMandiri <istora@mandiri.dev>
2022-10-18 10:33:23 +0000

    Add etc coop and etc labs to footer

--- src/components/socialIcons.js
+++ src/components/socialIcons.js
@@ -3,6 +3,7 @@ import tw from "twin.macro";
 
 import { useGlobals } from "../../plugins/translations-plugin/src/components/localizationProvider";
 import Icon from "./icon";
+import Tooltip from "./tooltip";
 
 export default function SocialIcons() {
   const {
@@ -11,30 +12,35 @@ export default function SocialIcons() {
   return (
     <div tw="space-y-4">
       <div tw="text-xs font-bold text-secondary-neutral">{socialHilight}</div>
-      <div tw="flex space-x-6">
+      <div tw="flex space-x-4">
         {socialItems.map(({ key, name, icon, link, hilight }) => (
-          <a
-            key={key}
-            target="_blank"
-            rel="noreferrer"
-            href={link}
-            css={[
-              hilight
-                ? tw`text-secondary-neutral hover:text-secondary-dark`
-                : tw`text-shade-light hover:text-shade-neutral`,
-            ]}
-          >
-            <span tw="sr-only">{name}</span>
-            <div tw="relative">
-              {hilight && (
-                <Icon
-                  icon="down"
-                  tw="absolute h-4 w-full -top-3 animate-bounce"
-                />
-              )}
-              <Icon icon={icon} tw="h-6" />
-            </div>
-          </a>
+          <div className="group" tw="relative">
+            <a
+              key={key}
+              target="_blank"
+              rel="noreferrer"
+              href={link}
+              css={[
+                hilight
+                  ? tw`text-secondary-neutral hover:text-secondary-dark`
+                  : tw`text-shade-light hover:text-shade-neutral`,
+              ]}
+            >
+              <span tw="sr-only">{name}</span>
+              <div tw="relative">
+                {hilight && (
+                  <Icon
+                    icon="down"
+                    tw="absolute h-4 w-full -top-3 animate-bounce"
+                  />
+                )}
+                <Icon icon={icon} tw="h-6" />
+              </div>
+            </a>
+            <Tooltip>
+              {name}
+            </Tooltip>
+          </div>
         ))}
       </div>
     </div>
